98dabd7cafe1f7b26fecfe6b45893c5ac7a403cd
anannanur <anannaanu974@gmail.com>
2022-05-25 20:55:14 +0600

    env file has been created

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,49 +1,252 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { Transition } from "@headlessui/react";
+import CustomLink from '../CustomLink/CustomLink';
+// import logo from '../../Assets/Images/logo.png'
+import { Link, useNavigate } from 'react-router-dom';
+// import { BsFacebook, BsTwitter, BsInstagram } from 'react-icons/bs';
+import { useAuthState } from 'react-firebase-hooks/auth';
+// import auth from '../../firebase.init';
+import { toast } from 'react-toastify';
+import { signOut } from 'firebase/auth';
+import auth from '../../firebase.init';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const user = useAuthState(auth);
+    // console.log(user);
+    const navigate = useNavigate();
+    //Signout------->
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                toast.success("Log Out successful", {
+                    toastId: "signOutSuccess"
+                });
+                navigate('/login');
+            })
+        localStorage.removeItem('token');
+    }
     return (
-        <div class="navbar bg-base-100">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li tabindex="0">
-                            <a class="justify-between">
-                                Parent
-                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </a>
-                            <ul class="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
+        <div className=' sticky top-0 z-50 shadow-sm'>
+            <div className="bg-gradient-to-b from-primary to-secondary hidden md:block">
+                <div className="flex justify-between items-center h-12 container mx-auto">
+                    <div className=" text-white text-md">
+                        <p>Get Free Shipping – Free 30 Day Money Back Guarantee</p>
+                    </div>
+                    <div className="right-icon flex text-white">
+                        <Link to="https://www.facebook.com/ayon.jodder.75/"><BsFacebook className='ml-8 text-xl'></BsFacebook></Link>
+                        <Link to="https://github.com/AyonJD"><BsTwitter className='ml-8 text-xl'></BsTwitter></Link>
+                        <Link to="https://github.com/AyonJD"><BsInstagram className='ml-8 text-xl'></BsInstagram></Link>
+                    </div>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal p-0">
-                    <li><a>Item 1</a></li>
-                    <li tabindex="0">
-                        <a>
-                            Parent
-                            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul class="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-            </div>
-            <div class="navbar-end">
-                <a class="btn">Get started</a>
-            </div>
+
+            <nav className="bg-[#F5F5F5] py-4">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex items-center justify-between md:block">
+                        <div className="flex items-center">
+                            <div className="flex-shrink-0 w-fit">
+                                <img onClick={() => navigate('/')}
+                                    className="cursor-pointer"
+                                    src={logo}
+                                    alt="Workflow"
+                                />
+                            </div>
+                            <div className="hidden md:flex justify-between items-center md:ml-auto">
+                                <div className="nav-item ml-10 flex items-baseline space-x-4 text-lg">
+                                    <CustomLink
+                                        to="/"
+                                        className="nav-btn px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Home
+                                    </CustomLink>
+                                    <CustomLink
+                                        to="/tools"
+                                        className="nav-btn px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Tools
+                                    </CustomLink>
+
+                                    {
+                                        user[0] && (
+                                            <>
+                                                <CustomLink
+                                                    to="/dashboard"
+                                                    className="nav-btn px-3 py-2 rounded-md text-sm font-medium"
+                                                >
+                                                    Dashboard
+                                                </CustomLink>
+
+                                                {/* <CustomLink
+                                                    to="add-item"
+                                                    className="nav-btn px-3 py-2 rounded-md text-sm font-medium"
+                                                >
+                                                    Add Item
+                                                </CustomLink> */}
+
+                                                {/* <CustomLink
+                                                    to="my-items"
+                                                    className="nav-btn px-3 py-2 rounded-md text-sm font-medium"
+                                                >
+                                                    My Items
+                                                </CustomLink> */}
+                                            </>
+                                        )
+                                    }
+
+                                    <CustomLink
+                                        to="/blog"
+                                        className="nav-btn px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Blog
+                                    </CustomLink>
+
+                                    <CustomLink
+                                        to="portfolio"
+                                        className="nav-btn px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Portfolio
+                                    </CustomLink>
+
+                                    <CustomLink
+                                        to="contact-us"
+                                        className="nav-btn px-3 py-2 rounded-md text-sm font-medium"
+                                    >
+                                        Contact
+                                    </CustomLink>
+                                </div>
+                                {
+                                    user[0] ? <button onClick={handleSignOut} className='md:ml-24 text-white bg-gradient-to-r from-primary to-secondary border-2 border-secondary hover:border-2 hover:border-primary hover:bg-gradient hover:from-white hover:to-white hover:text-primary transition-all transition-duration:150ms font-medium hover:font-medium px-5 py-1 rounded-md'>Logout</button> : <button onClick={() => navigate('/login')} className='md:ml-24 text-white bg-gradient-to-r from-primary to-secondary border-2 border-secondary hover:border-2 hover:border-primary hover:bg-gradient hover:from-white hover:to-white hover:text-primary  transition-all transition-duration:150ms font-medium hover:font-medium px-5 py-1 rounded-md'>Login</button>
+                                }
+                            </div>
+                        </div>
+                        <div className="-mr-2 flex md:hidden">
+                            {
+                                user[0] ? <button onClick={handleSignOut} className='md:ml-24 text-sm md:hidden block text-white bg-gradient-to-r from-primary to-secondary border-2 border-secondary hover:border-2 hover:border-primary hover:bg-gradient hover:from-white hover:to-white hover:text-primary transition-all transition-duration:150ms font-medium hover:font-medium px-3 py-1 rounded-md'>Logout</button> : <button onClick={() => navigate('/login')} className='md:ml-24 text-sm md:hidden block text-white bg-gradient-to-r from-primary to-secondary border-2 border-secondary hover:border-2 hover:border-primary hover:bg-gradient hover:from-white hover:to-white hover:text-primary transition-all transition-duration:150ms font-medium hover:font-medium px-3 py-1 rounded-md'>Login</button>
+                            }
+                            <button
+                                onClick={() => setIsOpen(!isOpen)}
+                                type="button"
+                                className="bg-gray-900 ml-5 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                aria-controls="mobile-menu"
+                                aria-expanded="false"
+                            >
+                                <span className="sr-only">Open main menu</span>
+                                {!isOpen ? (
+                                    <svg
+                                        className="block h-6 w-6"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        className="block h-6 w-6"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <Transition
+                    show={isOpen}
+                    enter="transition ease-out duration-800 transform"
+                    enterFrom="opacity-0 scale-50"
+                    enterTo="opacity-100 scale-100"
+                    leave="transition ease-in duration-75 transform"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                >
+                    {(ref) => (
+                        <div className="md:hidden" id="mobile-menu">
+                            <div ref={ref} className="mobile-nav px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                                <CustomLink
+                                    to="/"
+                                    className="hover:bg-gray-700 hover:text-white text-white block px-3 py-2 rounded-md text-base font-medium"
+                                >
+                                    Home
+                                </CustomLink>
+                                <CustomLink
+                                    to="/tools"
+                                    className="hover:bg-gray-700 hover:text-white text-white block px-3 py-2 rounded-md text-base font-medium"
+                                >
+                                    Tools
+                                </CustomLink>
+
+                                {
+                                    user[0] && (
+                                        <>
+                                            <CustomLink
+                                                to="/dashboard"
+                                                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                            >
+                                                Dashboard
+                                            </CustomLink>
+
+                                            {/* <CustomLink
+                                                to="/add-item"
+                                                className="text-white hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                            >
+                                                Add Item
+                                            </CustomLink> */}
+
+                                            {/* <CustomLink
+                                                to="/my-items"
+                                                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                            >
+                                                My Items
+                                            </CustomLink> */}
+                                        </>
+                                    )
+                                }
+
+                                <CustomLink
+                                    to="/blog"
+                                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                >
+                                    Blog
+                                </CustomLink>
+
+                                <CustomLink
+                                    to="portfolio"
+                                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                >
+                                    Portfolio
+                                </CustomLink>
+
+                                <CustomLink
+                                    to="/contact-us"
+                                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                >
+                                    Contact
+                                </CustomLink>
+                            </div>
+                        </div>
+                    )}
+                </Transition>
+            </nav>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {
 
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+// import { ReactQueryDevtools } from 'react-query/devtools'
+
+
+// Create a client
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
