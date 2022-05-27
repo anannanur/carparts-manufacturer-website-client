899150f6c546762f1fa4bf425f0ec9d55ea6d22c
anannanur <anannaanu974@gmail.com>
2022-05-27 12:50:25 +0600

    mistake fix

--- src/App.js
+++ src/App.js
@@ -30,41 +30,7 @@ function App() {
           </RequireAuth>
         }>
     </Route>
-    <Route path="dashboard" element={<RequiredAuth>
-          <Dashboard />
-        </RequiredAuth>} >
-          <Route index element={<Welcome></Welcome>}></Route>
-          <Route path='/dashboard/myorder' element={<MyOrder></MyOrder>}></Route>
-          <Route path="add-review" element={<AddReview></AddReview>}></Route>
-          <Route path="manageTools" element={
-            <RequireAdmin>
-              <ManageTools></ManageTools>
-            </RequireAdmin>
-          }></Route>
-          <Route path="my-profile" element={<MyProfile></MyProfile>}></Route>
-          <Route path="my-profile/edit-profile" element={<UpdateProfile></UpdateProfile>}></Route>
-          <Route path="user" element={
-            <RequireAdmin>
-              <AllUser></AllUser>
-            </RequireAdmin>
-          }></Route>
-          <Route path="manageOrder" element={
-            <RequireAdmin>
-              <ManageOrder></ManageOrder>
-            </RequireAdmin>
-          }></Route>
-          <Route path="add" element={
-            <RequireAdmin>
-              <AddTools></AddTools>
-            </RequireAdmin>
-          }></Route>
-          <Route path="tools/:id" element={
-            <RequireAdmin>
-              <UpdateTool></UpdateTool>
-            </RequireAdmin>
-          }></Route>
-          <Route path="payment/:id" element={<Payment />}></Route>
-        </Route>
+    
      </Routes>
      <Footer/>
      <ToastContainer></ToastContainer>
